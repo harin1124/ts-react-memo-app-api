326bff720e4cd8f16f94bde4d5fedd55d9407e29
harin1124 <lolrin1124@gmail.com>
2023-01-23 17:23:28 +0900

    인터셉터 개선

--- src/main/java/react/memo/api/config/CustomInterceptor.java
+++ src/main/java/react/memo/api/config/CustomInterceptor.java
@@ -9,7 +9,11 @@ import javax.servlet.http.HttpServletResponse;
 @Configuration
 public class CustomInterceptor implements HandlerInterceptor {
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-		System.out.println("check interceptor");
+		System.out.println("====================================");
+		System.out.println("CustomInterceptor");
+		System.out.printf("METHOD [%s]\r\n", request.getMethod());
+		System.out.printf("URI [%s]\r\n", request.getRequestURI());
+		System.out.println("====================================");
 		return true;
 	}
 }
