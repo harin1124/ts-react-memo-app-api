82988ece9d256f64996dc2c9cc2ae270d1abe0df
harin1124 <lolrin1124@gmail.com>
2023-07-01 12:07:12 +0900

    메모 조회 로직 수정

--- src/main/java/react/memo/api/service/MemoService.java
+++ src/main/java/react/memo/api/service/MemoService.java
@@ -1,23 +1,26 @@
 package react.memo.api.service;
 
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import react.memo.api.dto.Memo;
 import react.memo.api.dto.MemoRepository;
 import react.memo.api.dto.Users;
+import react.memo.api.repository.UserRepository;
 
 import java.util.ArrayList;
 
 @Service
+@RequiredArgsConstructor
 public class MemoService {
-	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
+  private final MemoRepository memoRepository;
+  private final UserRepository userRepository;
 
-    @Autowired
-    private MemoRepository memoRepository;
-    
-    public ArrayList<Memo> getMemoList(Users paramUser){
-        return memoRepository.findByOwnerSeq(paramUser.getUserSeq());
-    }
+  public ArrayList<Memo> getMemoList(String userId){
+    // 아이디로 유저 정보 가져오기
+    Users user = userRepository.findByUserId(userId);
+    return memoRepository.findByOwnerSeq(user.getUserSeq());
+  }
 }
